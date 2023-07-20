8ea259f3c9dd91826e550bd2f0feaba81b0cb6c2
Anita <anitatelagi.xworkz.com>
2023-07-20 11:34:53 +0530

    first prgm

---
+++ BalloonInfo.java
@@ -0,0 +1,22 @@
+class BalloonInfo{
+    public static void main(String[] args)
+  {
+	  short price=400;
+	  String gas="helium";
+	  String component="rubber";
+	  String type="gas ballon";
+	  byte size=12;
+	  String color="pink";
+	  
+	  
+	  System.out.println("Price:"+price);
+	  System.out.println("Gas:"+gas);
+      System.out.println("Component:"+component);
+ 	  System.out.println("type:"+type);
+   	  System.out.println("Size:"+size);
+	  System.out.println("Color:"+color);
+	  
+	  
+  }
+
+}
