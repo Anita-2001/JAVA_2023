e8228421b3e44ef8e8f381433e3a8933027027ee
Anita <anitatelagi.xworkz.com>
2023-07-20 11:50:59 +0530

    second code

---
+++ Cities.java
@@ -0,0 +1,34 @@
+class Cities{
+  public static void main(String[] args)
+  {
+    String Haveri="Haveri";
+	String Davanagere="Davanagere";
+	String Bidar="Bidar";
+	String Gadag="Gadag";
+	String Mysur="Mysur";
+	String Shivamogga="Shivamogga";
+	String Hubballi="Hubballi";
+	String Dharwad="Dharwad";
+    String[] cities={Haveri, Davanagere,Bidar ,Gadag,Mysur,Shivamogga,Hubballi,Dharwad};
+	
+	String ref1=cities[1];
+	System.out.println("element at index 1:"+ref1);
+	String ref2=cities[2];
+	System.out.println("element at index 2:"+ref2);
+	String ref3=cities[3];
+	System.out.println("element at index 3:"+ref3);
+	String ref4=cities[4];
+	System.out.println("element at index 4:"+ref4);
+	String ref5=cities[5];
+	System.out.println("element at index 5:"+ref5);
+	String ref6=cities[6];
+	System.out.println("element at index 6:"+ref6);
+	String ref7=cities[7];
+	System.out.println("element at index 7:"+ref7);
+	String ref8=cities[8];
+	System.out.println("element at index 8:"+ref8);
+  
+  
+  }
+
+}
